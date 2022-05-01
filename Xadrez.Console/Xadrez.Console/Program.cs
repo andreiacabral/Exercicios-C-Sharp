336ff5db297f3ec84ee73643deb2f1154ea6af0d
Andreia Costa <andreiacabraldacosta@outlook.com>
2022-05-01 14:41:27 -0300

    Criação do Projeto Xadrez

---
+++ Xadrez.Console/Xadrez.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_Console // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
+        }
+    }
+}
