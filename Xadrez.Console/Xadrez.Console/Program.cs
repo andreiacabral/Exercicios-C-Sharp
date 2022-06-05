4bcc227d268b7d8bf2911b24e7ecf38a802286dc
Andreia Costa <andreiacabraldacosta@outlook.com>
2022-06-05 16:25:16 -0300

    atualizações

--- Xadrez.Console/Xadrez.Console/Program.cs
+++ Xadrez.Console/Xadrez.Console/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez_Console // Note: actual namespace depends on the project name.
 {
      class Program
     {
         static void Main(string[] args)
-        {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.imprimirTabuleiro(tab);
+
+
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e) { 
+                Console.WriteLine(e.Message);
+            
+            }
             Console.ReadLine();
         }
     }
--- Xadrez.Console/Xadrez.Console/tabuleiro/Cor.cs
+++ Xadrez.Console/Xadrez.Console/tabuleiro/Cor.cs
@@ -8,7 +8,7 @@ namespace tabuleiro
 {
     public enum Cor
     {
-        anca,
+        Branca,
         Preta,
         Amarela,
         Azul,
