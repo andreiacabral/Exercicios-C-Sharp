70ca9cb5ef6063dab109dd341d0ebb31fa0d2405
Andreia Costa <andreiacabraldacosta@outlook.com>
2022-08-06 18:48:04 -0300

    Chave estrangeira não nula
     (integridade referencial)

---
+++ SalesWebMVC/SalesWebMvc/SalesWebMvc/Migrations/20220806213643_DepartmentForeignKey.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_salesRecords_Seller_SellerId",
+                table: "salesRecords");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_salesRecords",
+                table: "salesRecords");
+
+            migrationBuilder.RenameTable(
+                name: "salesRecords",
+                newName: "SalesRecords");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_salesRecords_SellerId",
+                table: "SalesRecords",
+                newName: "IX_SalesRecords_SellerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SalesRecords",
+                table: "SalesRecords",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecords_Seller_SellerId",
+                table: "SalesRecords",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecords_Seller_SellerId",
+                table: "SalesRecords");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SalesRecords",
+                table: "SalesRecords");
+
+            migrationBuilder.RenameTable(
+                name: "SalesRecords",
+                newName: "salesRecords");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SalesRecords_SellerId",
+                table: "salesRecords",
+                newName: "IX_salesRecords_SellerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_salesRecords",
+                table: "salesRecords",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_salesRecords_Seller_SellerId",
+                table: "salesRecords",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
