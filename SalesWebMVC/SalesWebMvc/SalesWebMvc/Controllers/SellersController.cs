7e37b65108b775a85ae374052133750c8bbd738c
Andreia Costa <andreiacabraldacosta@outlook.com>
2022-08-07 08:59:55 -0300

    Miniatura da aula 17:36 / 18:05 269.

--- SalesWebMVC/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,9 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SalesWebMvc.Services.Exception;
 
 namespace SalesWebMvc.Controllers
 {
@@ -12,7 +15,7 @@ namespace SalesWebMvc.Controllers
 
         private readonly DepartmentService _departmentService;
 
-        public  SellersController (SellerService sellerService,DepartmentService departmentService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerservice = sellerService;
             _departmentService = departmentService;
@@ -25,14 +28,13 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            var departments= _departmentService.FindAll();
+            var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public IActionResult Create(Seller seller)
         {
             _sellerservice.Insert(seller);
@@ -50,7 +52,7 @@ namespace SalesWebMvc.Controllers
             if (obj == null)
             {
                 return NotFound();
-            } 
+            }
             return View(obj);
         }
 
@@ -59,8 +61,9 @@ namespace SalesWebMvc.Controllers
         public IActionResult Delete(int id)
         {
             _sellerservice.Remove(id);
-            return RedirectToAction(nameof(Index)); 
+            return RedirectToAction(nameof(Index));
         }
+
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -74,6 +77,49 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerservice.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+
+            }
+            try
+            {
+                _sellerservice.Update(seller);
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
